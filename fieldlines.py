8642918f44f2b8b01e3d1c36bae151c92c362c2c
Hector Tiblom <hectortib@gmail.com>
2020-06-15 01:28:00 +0200

    Added markers for charges and changed check near the positions of charges

--- fieldlines.py
+++ fieldlines.py
@@ -19,34 +19,49 @@ class Field_lines:
 
     def calculate_lines(self):
         with np.errstate(divide='raise', invalid='raise'):
-            neg_pos = np.array([i.pos for i in self.charges if i.q < 0])
+            positions = np.array([i.pos for i in self.charges if i.q])
             lines_arr = []
             if (np.all(np.sign([i.q for i in self.charges]) == np.sign(self.charges[0].q))):
                 # All charges have the same sign, so no lines end
                 pass 
             for n in self.charges:
-                if n.q > 0:
-                    for i in range(self.num_of_lines):
-                        r = n.pos + np.array([np.cos(i*2*np.pi/self.num_of_lines), np.sin(i*2*np.pi/self.num_of_lines)])*self.step
-                        line = [[r[0], r[1]]]
-                        while np.abs(r[0]) <= self.max_x and np.abs(r[1]) <= self.max_y:
-                            if len(neg_pos):
-                                if np.any(np.array([la.norm(i) for i in r-neg_pos]) < self.step):
+                if not n.q:
+                    continue
+                for i in range(self.num_of_lines):
+                    r = n.pos + np.array([np.cos(i*2*np.pi/self.num_of_lines), np.sin(i*2*np.pi/self.num_of_lines)])*self.step
+                    line = [[r[0], r[1]]]
+                    direction = 1
+                    if n.q < 0:
+                        direction = -1
+                    print(r + self.total_field(r)/la.norm(self.total_field(r))*self.step*direction)
+                    r += self.total_field(r)/la.norm(self.total_field(r))*self.step*direction
+                    line.append([r[0], r[1]])
+                    while np.abs(r[0]) <= self.max_x and np.abs(r[1]) <= self.max_y:
+                        if np.any(np.array([la.norm(i) for i in r-positions]) < 0.5*self.step):
+                            break
+                        with np.errstate(divide='raise', invalid='raise'):
+                            try:
+                                r += self.total_field(r)/la.norm(self.total_field(r))*self.step
+                                if np.allclose(r, line[-2]):
                                     break
-                            with np.errstate(divide='raise', invalid='raise'):
-                                try:
-                                    r += self.total_field(r)/la.norm(self.total_field(r))*self.step
-                                    line.append([r[0], r[1]])
-                                except FloatingPointError:
-                                    break
-                        lines_arr.append(np.array(line))
+                                line.append([r[0], r[1]])
+                            except FloatingPointError:
+                                break
+                    lines_arr.append(np.array(line))
             lines_arr = np.array(lines_arr)
             return lines_arr
         
-    def plot_lines(self):
+    def plot(self):
         fig, ax = plt.subplots()
         ax.set_xlim([-self.max_x, self.max_x])
         ax.set_ylim([-self.max_y, self.max_y])
         lines = self.calculate_lines()
         for i in lines:
             ax.plot(i[:,0], i[:,1], color='black')
+        for n in self.charges:
+            if n.q > 0:
+                ax.plot(*n.pos, color='red', marker='o', markersize='15')
+            elif n.q < 0:
+                ax.plot(*n.pos, color='blue', marker='o', markersize='15')
+            else:
+                ax.plot(*n.pos, color='grey', marker='o', markersize='15')
--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import charge
 import fieldlines
 
-poscharge = charge.Charge(1, [-0.5,0])
-negcharge = charge.Charge(-1, [0.5,0])
+poscharge = charge.Charge(0, [-1,0])
+negcharge = charge.Charge(-1, [1,0])
 
 charges = [poscharge, negcharge]
         
-l = fieldlines.Field_lines(charges, max_x=2, max_y=2, num_of_lines=20, step=0.05)
-l.plot_lines()
+l = fieldlines.Field_lines(charges, max_x=2, max_y=2, num_of_lines=30, step=0.05)
+l.plot()
 plt.show()
